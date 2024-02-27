ddf753bfbf363e4366c7e43fe271d1dde1e946ef
Cesar Fernandez <CesarFernandez2@live.com>
2024-02-27 15:52:20 -0700

    Initial Commit

---
+++ Text ValidationUITests/Text_ValidationUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Text_ValidationUITestsLaunchTests.swift
+//  Text ValidationUITests
+//
+//  Created by Cesar Fernandez on 2/27/24.
+//
+
+import XCTest
+
+final class Text_ValidationUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
