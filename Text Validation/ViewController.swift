145cd8a8a5a23c49230078a472b5d703905422e3
Cesar Fernandez <CesarFernandez2@live.com>
2024-02-28 16:23:14 -0700

    Text Validation Initial Commit

--- Text Validation/ViewController.swift
+++ Text Validation/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passWordTextField: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var successView: UIStackView!
+    @IBOutlet weak var errorView: UIStackView!
+    @IBOutlet weak var introScreenView: UIStackView!
+    
+    let errorViewIsShowing = false
+    let successViewIsShowing = false
+    let introduceScreenViewIsShowing = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+            errorView.isHidden = true
+            successView.isHidden = true
+            introScreenView.isHidden = false
     }
-
-
+    
+    let specialSymbols = ["@",",","!","#","$","%","^","&","*","<",">","?","+","=","-"]
+    
+    @IBAction func submitButtonPressed(_ sender: UIButton) {
+        if let password = passWordTextField.text {
+            if containsSpecialSymbol(password) {
+                showSuccessView()
+            } else {
+                showErrorView()
+            }
+    }
+    
+        func containsSpecialSymbol(_ password: String) -> Bool {
+        for char in password {
+            if specialSymbols.contains(String(char)) { return true }
+            }
+            return false
+        }
+        
+        func showErrorView() {
+            errorView.isHidden = false
+            successView.isHidden = true
+            introScreenView.isHidden = true
+        }
+        
+        func showSuccessView() {
+            errorView.isHidden = true
+            successView.isHidden = false
+            introScreenView.isHidden = true
+        }
+    }
 }
-
